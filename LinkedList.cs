cca3f082ec1e3502e85d00c5ecc0ae7fb11204e3
johnzon <johnsonbayoadekunle@gmail.com>
2020-01-06 17:13:28 +0100

    Update with Comment

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,4 +1,8 @@
- public class LinkedList
+/*
+An implementation of Single Link list class  with cosntructor.
+*/ 
+
+public class LinkedList
     {
         SinglyLinkedListNode  head;
 
@@ -7,6 +11,10 @@
             head = _head;
         }
 
+ /*
+Append = add the data to the end of the current tail i.e the last elemtn of the list. if the
+list currently has no element.The data is set as the head
+*/ 
         public void append(int data)
         {
             if(head==null)
@@ -23,7 +31,11 @@
                 current.next = new SinglyLinkedListNode(data);
             }
         }
-
+ /*
+Prepend  add the data to the beginning of the current head i.e the first elemtn of the list. if the
+list currently has no element.The data is set as the head.
+The current head is set as the next pointer to the head.
+*/ 
         public void prepend(int data)
         {
              if(head==null)
@@ -37,14 +49,20 @@
                 head = current;
             }
         }
+ /*
+Delete value remove the reference to the node having the current specified data.
 
+*/ 
         public void DeleteValue(int data)
         {
             if(head ==null)
             {
                 return;
             }
-            if(head.data == data)
+         //If the head has the data, set head to the current next element to head
+         //else, starting from current's next which will not be the head.
+         //update the pointer to point to the current next's next element
+         if(head.data == data)
             {
                 head = head.next;
             }
@@ -53,6 +71,7 @@
                 var current = head;
                 while(current.next!=null)
                 {
+                  //current.next ensure that we don't delete head data in this logic because it has been handled above
                     if(current.next.data == data)
                     {
                         current.next = current.next.next;
